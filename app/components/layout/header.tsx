184c126179015ab3affa11e482039bee93ec618e
kurtsiwi <matias.villegas.varas@gmail.com>
2025-12-05 20:29:28 -0300

    feat: update Header component with NavigationMenu and add RegistrationFormPage

--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -1,11 +1,31 @@
+import Link from "next/link";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuIndicator,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  NavigationMenuViewport,
+} from "@/components/ui/navigation-menu";
+
 export default function Header() {
   return (
     <header className="w-full bg-white/50 backdrop-blur-sm sticky top-0 z-50 border-b border-zinc-200 dark:bg-black/50 dark:border-zinc-800">
-      <div className="mx-auto flex h-16 max-w-3xl items-center px-4 sm:px-6 lg:px-8">
-        <h1 className="text-xl font-semibold text-zinc-900 dark:text-zinc-100">
-          Candy Farms
-        </h1>
-      </div>
+      <NavigationMenu>
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <Link href="#">LOGO</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <Link href="#">Hazte Socio</Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+      </NavigationMenu>
     </header>
   );
 }

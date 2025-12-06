184ea23c8d6cbc088af2ba44e27e2839d0dad9d6
kurtsiwi <matias.villegas.varas@gmail.com>
2025-12-05 22:05:53 -0300

    feat: enhance button styles and layout adjustments in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
 
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-8 bg-white dark:bg-black sm:items-start">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-8 bg-white dark:bg-black sm:items-start lg:justify-start xl:px-0 gap-8">
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <p className="max-w-md text-base leading-4 text-zinc-600 dark:text-zinc-400 ">
             Nos alegra anunciar que ya abrimos oficialmente las puertas de

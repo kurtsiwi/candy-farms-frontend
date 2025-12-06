c05901c73dcbd813d1f5dc753eeeabe685a19c30
kurtsiwi <matias.villegas.varas@gmail.com>
2025-12-05 21:16:57 -0300

    feat: add WhatsApp icon and update footer and header components

--- app/components/layout/footer.tsx
+++ app/components/layout/footer.tsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="w-full bg-white/50 backdrop-blur-sm border-t border-zinc-200 dark:bg-black/50 dark:border-zinc-800">
       <div className="mx-auto flex h-16 max-w-3xl items-center justify-center px-4 sm:px-6 lg:px-8">
         <p className="text-sm text-zinc-600 dark:text-zinc-400">
-          &copy; {new Date().getFullYear()} Candy Farms. All rights reserved.
+          &copy; {new Date().getFullYear()} Candy Farms
         </p>
       </div>
     </footer>
--- app/components/layout/header.tsx
+++ app/components/layout/header.tsx
@@ -16,19 +16,25 @@ import {
 
 export default function Header() {
   return (
-    <header className="w-full bg-white/50 backdrop-blur-sm sticky top-0 z-50 border-b border-zinc-200 dark:bg-black/50 dark:border-zinc-800">
-      <NavigationMenu>
+    <header className="w-full bg-white/50 backdrop-blur-sm sticky top-0 z-50 border-b border-zinc-200 dark:bg-black/50 dark:border-zinc-800 list-none">
+      <NavigationMenu className="max-w-3xl justify-around">
         <NavigationMenuItem>
           <NavigationMenuLink asChild>
             <Link href="#">
-            <Image
-              src={CandyFarmsLogo}
-              alt="Candy Farms Logo"
-              width={120}
-              height={40}
-              className="m-2"
-            />
+              <Image
+                src={CandyFarmsLogo}
+                alt="Candy Farms Logo"
+                width={80}
+                className="m-2"
+              />
             </Link>
+          </NavigationMenuLink>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <NavigationMenuLink asChild>
+            <h2 className="max-w-xs text-2xl font-semibold tracking-tight text-black dark:text-zinc-50">
+              ¡El dispensario ya está funcionando!
+            </h2>
           </NavigationMenuLink>
         </NavigationMenuItem>
       </NavigationMenu>
